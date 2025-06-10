d8d1ae078a303a6242727a7d0d7a3c469ca10fa4
Erick <erickedu211@gmail.com>
2025-06-10 19:14:04 -0300

    Organização e estilização

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ var apiKey = "a65dbb2240a449bd8e7183620242804";
 
 function atualizaDadosCidade(dadosTempoCidade) {
     let cityCard = $("#" + dadosTempoCidade.location.name + "Card");
-    cityCard.find(".cityName").text(dadosTempoCidade.location.name);
-    cityCard.find(".temp").html(dadosTempoCidade.current.temp_c + "&deg;");
-    cityCard.find(".condition").html('<span>Clima</span><span>' + dadosTempoCidade.current.condition.text + '</span>');
-    cityCard.find(".sensacao").html("<span>Sensação Térmica</span><span>" + dadosTempoCidade.current.feelslike_c + "</span>");
+    cityCard.find(".nomeCidade").text(dadosTempoCidade.location.name);
+    cityCard.find(".tempo").html(dadosTempoCidade.current.temp_c + "&deg;");
+    cityCard.find(".condicao").html('<span>Clima:</span><span>' + dadosTempoCidade.current.condition.text + '</span>');
+    cityCard.find(".sensacao").html("<span>Sensação Térmica:</span><span>" + dadosTempoCidade.current.feelslike_c + "</span>");
 }
 
 function populaPagina() {
-    $(".cityName").each(function() {
+    $(".nomeCidade").each(function() {
         let nomeCidade = $(this).text();
         $.ajax({
             url: "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + nomeCidade + "&aqi=no&lang=pt",
@@ -21,22 +21,22 @@ function populaPagina() {
 $(document).ready(function() {
     populaPagina();
     
-    $("#btnAddCity").click(function() {
+    $("#btnAdicionarCidade").click(function() {
         let novaCidade = $("#txtCidade").val().trim();
         if (novaCidade) {
             let novoCard = `
                 <div class="col-md-4">
-                    <div class="card wheaterCard" id="${novaCidade}Card">
+                    <div id="${novaCidade}Card" class="card wheaterCard">
                         <div class="card-header">
                             <h2 class="h5 mb-0 text-center">
                                 Tempo agora em<br>
-                                <span class="cityName fst-italic">${novaCidade}</span>
+                                <span class="nomeCidade fst-italic">${novaCidade}</span>
                             </h2>
                         </div>
-                        <div class="card-body cityData">
-                            <p class="temp display-4 text-center">--&deg;</p>
-                            <p class="itemsCD condition d-flex justify-content-between"><span>Clima</span><span>--</span></p>
-                            <p class="itemsCD sensacao d-flex justify-content-between"><span>Sensação Térmica</span><span>--</span></p>
+                        <div class="card-body dadosCidade">
+                            <p class="tempo display-4 text-center">--&deg;</p>
+                            <p class="itemsCD condicao d-flex justify-content-evenly"><span>Clima</span><span>--</span></p>
+                            <p class="itemsCD sensacao d-flex justify-content-evenly"><span>Sensação Térmica</span><span>--</span></p>
                         </div>
                     </div>
                 </div>`;
